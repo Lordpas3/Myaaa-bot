1a9ef3204af6d1e222c37fc6a2215929ba6246e5
Lordpas3 <lordpas3@gmail.com>
2020-03-11 15:40:17 +0100

    MYAAA

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
- const client = new Discord.Client();
+const client = new Discord.Client();
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
  });
 
+
 client.on('message', msg => {
- if (msg.content === 'ping') {
- msg.reply('pong');
+ if (msg.content === 'MYAAA') {
+ msg.channel.send("", { files: ["./MYAAA.gif"] });
  }
  });
 
-client.login('wAturzaF_cd07dzOsr2IYAKudHbEUmHC');
+client.login('Njg3MjkxMDM2NjM2NDEzOTY1.XmjujA.WSaTha1vBKjRkJeX1kWP3BmTvbY');
