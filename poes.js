4839ec1769b28427356690500179cd3ba359c22b
Lordpas3 <lordpas3@gmail.com>
2020-03-12 15:19:25 +0100

    fixed duplication

--- poes.js
+++ poes.js
@@ -15,21 +15,21 @@ client.on('message', msg => {
 
 // Database connection
 
-const { Client } = require('pg');
+const { DBClient } = require('pg');
 
-const client = new Client({
+const dbclient = new DBClient({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
-client.connect();
+dbclient.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+dbclient.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();
+  dbclient.end();
 });
 
 client.login(process.env.BOT_TOKEN);
