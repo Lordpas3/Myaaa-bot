907e727f796e218a750ca503a8687ea11b555210
Lordpas3 <lordpas3@gmail.com>
2020-03-12 15:16:57 +0100

    fix?

--- poes.js
+++ poes.js
@@ -15,12 +15,21 @@ client.on('message', msg => {
 
 // Database connection
 
-var mysql = require('mysql');
+const { Client } = require('pg');
 
-var con = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
 
 client.login(process.env.BOT_TOKEN);
