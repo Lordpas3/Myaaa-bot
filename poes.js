27fc926cec41a5c79d863e66ce0fc80e7a475f04
Lordpas3 <lordpas3@gmail.com>
2020-03-12 15:30:02 +0100

    fuck postgresql

--- poes.js
+++ poes.js
@@ -12,24 +12,4 @@ client.on('message', msg => {
  }
  });
 
-
-// Database connection
-
-const { DBClient } = require('pg');
-
-const dbclient = new DBClient({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-dbclient.connect();
-
-dbclient.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  dbclient.end();
-});
-
 client.login(process.env.BOT_TOKEN);
