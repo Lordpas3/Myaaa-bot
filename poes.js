38be68344bd966414617de3e3c86771fabdbab42
Lordpas3 <lordpas3@gmail.com>
2020-08-31 23:10:44 +0200

    vibe

--- poes.js
+++ poes.js
@@ -2,14 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
- console.log(`Logged in as ${client.user.tag}!`);
- });
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
 
 client.on('message', msg => {
- if (msg.content === 'MYAAA') {
- msg.channel.send("", { files: ["./MYAAA.gif"] });
- }
- });
+    if (msg.content === 'MYAAA') {
+        msg.channel.send("", { files: ["./MYAAA.gif"] });
+    }
+    if (msg.content === 'vibe') {
+        msg.channel.send("", { files: ["./vibe.gif"] });
+    }
+});
 
 client.login(process.env.BOT_TOKEN);
